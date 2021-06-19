f3b8ee6e3d435b12b9932f19df236ef9fada2c61
Eric Bower <me@erock.io>
2021-06-19 11:07:04 -0400

    all endpoints converted

--- src/lists/effects.ts
+++ src/lists/effects.ts
@@ -1,7 +1,6 @@
-import { batch, call, createEffects, put, select } from 'redux-cofx';
+import { put, select } from 'redux-saga/effects';
 import { MapEntity } from 'robodux';
 
-import { apiFetch, ApiFetchResponse } from '@app/fetch';
 import {
   UpsertList,
   UpsertListItem,
@@ -19,16 +18,14 @@ import {
   ApiGen,
 } from '@app/types';
 import {
-  deleteItemLoader,
-  fetchListLoader,
-  Loaders,
-  setLoaderError,
-  setLoaderStart,
+  // deleteItemLoader,
+  // fetchListLoader,
   setLoaderSuccess,
 } from '@app/loaders';
 import { selectHasTokenExpired } from '@app/token';
 import { addUsers, processUsers } from '@app/users';
 import { processComments, addComments } from '@app/comments';
+import { api, ApiCtx } from '@app/api';
 
 import {
   deserializeList,
@@ -48,6 +45,9 @@ import {
   patchLists,
   addUsernameStarIds,
 } from './slice';
+
+const isEmpty = (obj: any) =>
+  Object.keys(obj).length === 0 && obj.constructor === Object;
 
 export function processLists(lists: ListResponse[]) {
   return lists.reduce<MapEntity<ListClient>>((acc, list) => {
@@ -74,412 +74,291 @@ export function processListItems(itemsArr: ListItemResponse[]) {
   return { items, itemIds };
 }
 
-function* onCreateList(payload: UpsertList) {
-  const loader = Loaders.createList;
-  yield put(setLoaderStart({ id: loader }));
-  const resp: ApiFetchResponse<BulkCreateListResponse> = yield call(
-    apiFetch,
-    '/lists',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        name: payload.name,
-        description: payload.description,
-        public: payload.public,
-        items: payload.items,
-        plugins: payload.plugins,
-      }),
-    },
-  );
+export const createList = api.post<UpsertList>('/lists', function* (
+  ctx: ApiCtx<BulkCreateListResponse, UpsertList>,
+  next,
+) {
+  ctx.request = {
+    body: JSON.stringify({
+      name: ctx.payload.name,
+      description: ctx.payload.description,
+      public: ctx.payload.public,
+      items: ctx.payload.items,
+      plugins: ctx.payload.plugins,
+    }),
+  };
+  yield next();
+  if (!ctx.response.ok) return;
 
-  if (!resp.ok) {
-    yield put(setLoaderError({ id: loader, message: resp.data.message }));
-    return;
-  }
-
-  const list = deserializeList(resp.data.list);
+  const { data } = ctx.response;
+  const list = deserializeList(data.list);
   const listIds: string[] = [];
-  const items = resp.data.items.reduce<MapEntity<ListItemClient>>(
-    (acc, item) => {
-      acc[item.id] = deserializeListItem(item);
-      listIds.push(item.id);
-      return acc;
-    },
-    {},
-  );
-  yield batch([
-    setLoaderSuccess({ id: loader, meta: { list } }),
+  const items = data.items.reduce<MapEntity<ListItemClient>>((acc, item) => {
+    acc[item.id] = deserializeListItem(item);
+    listIds.push(item.id);
+    return acc;
+  }, {});
+  ctx.actions.push(
+    setLoaderSuccess({ id: ctx.name, meta: { list } }),
     addLists({ [list.id]: list }),
     addListItems(items),
     addListItemIds({ [list.id]: listIds }),
-  ]);
-}
-
-function* onUpdateList(payload: UpsertList) {
-  const { id, ...data } = payload;
-  if (!id) {
-    return;
-  }
-
-  const loader = Loaders.updateList;
-
-  yield put(setLoaderStart({ id: loader }));
-  const resp: ApiFetchResponse<ListResponse> = yield call(
-    apiFetch,
-    `/lists/${id}`,
-    {
-      method: 'PUT',
-      body: JSON.stringify({
-        name: data.name,
-        description: data.description,
-        public: data.public,
-        plugins: data.plugins,
-      }),
-    },
   );
+});
 
-  if (!resp.ok) {
-    yield put(setLoaderError({ id: loader, message: resp.data.message }));
-    return;
-  }
-
-  const list = deserializeList(resp.data);
-  yield batch([
+export const updateList = api.put<UpsertList>('/lists/:id', function* (
+  ctx,
+  next,
+) {
+  if (!ctx.payload.id) return;
+  ctx.request = {
+    body: JSON.stringify({
+      name: ctx.payload.name,
+      description: ctx.payload.description,
+      public: ctx.payload.public,
+      plugins: ctx.payload.plugins,
+    }),
+  };
+  yield next();
+  if (!ctx.response.ok) return;
+  const list = deserializeList(ctx.response.data);
+  ctx.actions.push(
     addLists({ [list.id]: list }),
-    setLoaderSuccess({ id: loader, meta: { list } }),
-  ]);
-}
+    setLoaderSuccess({ id: ctx.name, meta: { list } }),
+  );
+});
 
-export function* onFetchLists(): ApiGen {
+export const fetchLists = api.get('/lists', function* (
+  ctx: ApiCtx<ApiListsResponse>,
+  next,
+): ApiGen {
   const hasTokenExpired = yield select(selectHasTokenExpired);
   if (hasTokenExpired) {
     return;
   }
 
-  const resp: ApiFetchResponse<ApiListsResponse> = yield call(
-    apiFetch,
-    '/lists',
-  );
+  yield next();
+  const { ok, data } = ctx.response;
+  if (!ok) return;
+  const users = processUsers(data.users);
+  const lists = processLists(data.lists);
+  ctx.actions.push(addLists(lists), addUsers(users));
+});
 
-  if (!resp.ok) {
-    return;
-  }
+export const fetchPublicLists = api.get('/lists/public', function* (
+  ctx: ApiCtx<ApiListsResponse>,
+  next,
+) {
+  yield next();
+  if (!ctx.response.ok) return;
+  const { data } = ctx.response;
+  const lists = processLists(data.lists);
+  const users = processUsers(data.users);
+  ctx.actions.push(addLists(lists), addUsers(users));
+});
 
-  const users = processUsers(resp.data.users);
-  const lists = processLists(resp.data.lists);
-  yield batch([addLists(lists), addUsers(users)]);
-}
+export const fetchStars = api.get<{ username: string }>(
+  '/users/:username/stars',
+  function* (ctx: ApiCtx<ApiStarsResponse>, next) {
+    const { username } = ctx.payload;
+    if (!username) return;
+    yield next();
+    if (!ctx.response.ok) return;
+    const { data } = ctx.response;
+    const stars = processStars(data.stars);
+    const lists = processLists(data.lists);
+    const starIds = data.stars.map((s) => s.id);
+    ctx.actions.push(
+      addStars(stars),
+      addLists(lists),
+      addUsernameStarIds({ [username]: starIds }),
+    );
+  },
+);
 
-export function* onFetchPublicLists() {
-  const resp: ApiFetchResponse<ApiListsResponse> = yield call(
-    apiFetch,
-    '/lists/public',
-  );
+export const startList = api.post<{ listId: string }>(
+  '/lists/:listId/star',
+  function* (ctx: ApiCtx<StarResponse>, next) {
+    const { listId } = ctx.payload;
+    yield next();
+    const { ok, data } = ctx.response;
+    if (!ok) return;
 
-  if (!resp.ok) {
-    return;
-  }
+    const list: ListClient = yield select(selectListById, { id: listId });
+    if (isEmpty(data)) {
+      ctx.actions.push(
+        removeStars([listId]),
+        patchLists({ [listId]: { stars: list.stars - 1 } }),
+      );
+    } else {
+      const star = deserializeStar(data);
+      ctx.actions.push(
+        addStars({ [star.listId]: star }),
+        patchLists({ [listId]: { stars: list.stars + 1 } }),
+      );
+    }
+  },
+);
 
-  const lists = processLists(resp.data.lists);
-  const users = processUsers(resp.data.users);
-  yield batch([addLists(lists), addUsers(users)]);
-}
-
-export function* onFetchStars(username: string) {
-  if (!username) {
-    return;
-  }
-
-  const resp: ApiFetchResponse<ApiStarsResponse> = yield call(
-    apiFetch,
-    `/users/${username}/stars`,
-  );
-
-  if (!resp.ok) {
-    return;
-  }
-
-  const stars = processStars(resp.data.stars);
-  const lists = processLists(resp.data.lists);
-  const starIds = resp.data.stars.map((s) => s.id);
-  yield batch([
-    addStars(stars),
-    addLists(lists),
-    addUsernameStarIds({ [username]: starIds }),
-  ]);
-}
-
-const isEmpty = (obj: any) =>
-  Object.keys(obj).length === 0 && obj.constructor === Object;
-
-function* onStarList(listId: string) {
-  const resp: ApiFetchResponse<StarResponse> = yield call(
-    apiFetch,
-    `/lists/${listId}/star`,
-    {
-      method: 'POST',
-    },
-  );
-
-  if (!resp.ok) {
-    return;
-  }
-
-  const list: ListClient = yield select(selectListById, { id: listId });
-  if (isEmpty(resp.data)) {
-    yield batch([
-      removeStars([listId]),
-      patchLists({ [listId]: { stars: list.stars - 1 } }),
-    ]);
-  } else {
-    const star = deserializeStar(resp.data);
-    yield batch([
-      addStars({ [star.listId]: star }),
-      patchLists({ [listId]: { stars: list.stars + 1 } }),
-    ]);
-  }
-}
-
-function* onCreateListItem(payload: UpsertListItem): ApiGen {
-  const { listId } = payload;
-  const loader = Loaders.createItem;
-
-  yield put(setLoaderStart({ id: loader }));
-
-  const resp: ApiFetchResponse<ListItemResponse> = yield call(
-    apiFetch,
-    `/lists/${listId}/items`,
-    {
-      method: 'POST',
+export const createListItem = api.post<UpsertListItem>(
+  '/lists/:listId/items',
+  function* (ctx: ApiCtx<ListItemResponse, UpsertListItem>, next): ApiGen {
+    const { listId } = ctx.payload;
+    ctx.request = {
       body: JSON.stringify({
-        text: payload.text,
+        text: ctx.payload.text,
       }),
-    },
-  );
+    };
+    yield next();
+    const { ok, data } = ctx.response;
+    if (!ok) return;
+    const listItem = deserializeListItem(data);
+    const itemIds = yield select(selectItemIdsByList, { id: listId });
+    ctx.actions.push(
+      addListItems({ [listItem.id]: listItem }),
+      addListItemIds({ [listId]: [...itemIds, listItem.id] }),
+      setLoaderSuccess({ id: ctx.name, message: listItem.id }),
+    );
+  },
+);
 
-  if (!resp.ok) {
-    yield put(setLoaderError({ id: loader }));
-    return;
-  }
-
-  const listItem = deserializeListItem(resp.data);
-  const itemIds = yield select(selectItemIdsByList, { id: listId });
-  yield batch([
-    addListItems({ [listItem.id]: listItem }),
-    addListItemIds({ [listId]: [...itemIds, listItem.id] }),
-    setLoaderSuccess({ id: loader, message: listItem.id }),
-  ]);
-}
-
-function* onCreateListItemBulk(payload: UpsertListItem): ApiGen {
-  const { listId } = payload;
-  const loader = Loaders.createItem;
-
-  yield put(setLoaderStart({ id: loader }));
-
-  const resp: ApiFetchResponse<BulkCreateListResponse> = yield call(
-    apiFetch,
-    `/lists/${listId}/bulk`,
-    {
-      method: 'POST',
+export const createListItemBulk = api.post<UpsertListItem>(
+  '/lists/:listId/bulk',
+  function* (
+    ctx: ApiCtx<BulkCreateListResponse, UpsertListItem>,
+    next,
+  ): ApiGen {
+    const { listId } = ctx.payload;
+    ctx.request = {
       body: JSON.stringify({
-        text: payload.text,
+        text: ctx.payload.text,
       }),
-    },
-  );
+    };
+    yield next();
+    const { ok, data } = ctx.response;
+    if (!ok) return;
+    const list = deserializeList(data.list);
+    const { items, itemIds } = processListItems(data.items);
+    const curItemIds = yield select(selectItemIdsByList, { id: listId });
+    ctx.actions.push(
+      addLists({ [list.id]: list }),
+      addListItems(items),
+      addListItemIds({
+        [listId]: [...curItemIds, ...itemIds],
+      }),
+    );
+  },
+);
 
-  if (!resp.ok) {
-    yield put(setLoaderError({ id: loader }));
-    return;
-  }
-
-  const list = deserializeList(resp.data.list);
-  const { items, itemIds } = processListItems(resp.data.items);
-  const curItemIds = yield select(selectItemIdsByList, { id: listId });
-  yield batch([
-    addLists({ [list.id]: list }),
-    addListItems(items),
-    addListItemIds({
-      [listId]: [...curItemIds, ...itemIds],
-    }),
-    setLoaderSuccess({ id: loader }),
-  ]);
-}
-
-export function* onUpdateListItem(payload: UpsertListItem) {
-  const { listId } = payload;
-  const { id, text, metadata, notes } = payload;
-  if (!id) {
-    return;
-  }
-
-  const loader = Loaders.updateListItem;
-  yield put(setLoaderStart({ id: loader }));
-  const resp: ApiFetchResponse<ListItemResponse> = yield call(
-    apiFetch,
-    `/lists/${listId}/items/${id}`,
-    {
-      method: 'PUT',
+export const updateListItem = api.put<UpsertListItem>(
+  '/lists/:listId/items/:id',
+  function* (ctx: ApiCtx<ListItemResponse, UpsertListItem>, next) {
+    const { id, text, metadata, notes } = ctx.payload;
+    if (!id) return;
+    ctx.request = {
       body: JSON.stringify({
         text,
         notes,
         metadata,
       }),
-    },
-  );
-
-  if (!resp.ok) {
-    yield put(setLoaderError({ id: loader, message: resp.data.message }));
-    return;
-  }
-
-  const listItem = deserializeListItem(resp.data);
-  yield batch([
-    addListItems({ [listItem.id]: listItem }),
-    setLoaderSuccess({ id: loader, message: listItem.id }),
-  ]);
-}
+    };
+    yield next();
+    const { ok, data } = ctx.response;
+    if (!ok) return;
+    const listItem = deserializeListItem(data);
+    ctx.actions.push(addListItems({ [listItem.id]: listItem }));
+  },
+);
 
 interface OrderListItems {
   listId: string;
   order: string[];
 }
 
-function* onOrderListItems(payload: OrderListItems): ApiGen {
-  const { listId, order } = payload;
-
-  const curListIds = yield select(selectItemIdsByList, { id: listId });
-
-  yield put(addListItemIds({ [listId]: order }));
-
-  const resp: ApiFetchResponse<ApiOrderResponse> = yield call(
-    apiFetch,
-    `/lists/${listId}/order`,
-    {
-      method: 'POST',
+export const orderListItems = api.post<OrderListItems>(
+  '/lists/:listId/order',
+  function* (ctx: ApiCtx<ApiOrderResponse, OrderListItems>, next): ApiGen {
+    const { listId, order } = ctx.payload;
+    const curListIds = yield select(selectItemIdsByList, { id: listId });
+    yield put(addListItemIds({ [listId]: order }));
+    ctx.request = {
       body: JSON.stringify({ order }),
-    },
-  );
+    };
+    yield next();
+    if (!ctx.response.ok) {
+      // undo
+      ctx.actions.push(addListItemIds({ [listId]: curListIds }));
+      return;
+    }
+  },
+);
 
-  if (!resp.ok) {
-    // undo
-    yield put(addListItemIds({ [listId]: curListIds }));
-  }
-}
-
-function* onDeleteList(listId: string): ApiGen {
-  const loader = Loaders.deleteList;
-  yield put(setLoaderStart({ id: loader }));
-  const resp: ApiFetchResponse = yield call(apiFetch, `/lists/${listId}`, {
-    method: 'DELETE',
-  });
-  if (!resp.ok) {
-    yield put(setLoaderError({ id: loader, message: resp.data.message }));
-    return;
-  }
-
-  const itemIds = yield select(selectItemIdsByList, { id: listId });
-  yield batch([
-    removeLists([listId]),
-    removeListItemIds([listId]),
-    removeListItems(itemIds),
-    setLoaderSuccess({ id: loader, message: listId }),
-  ]);
-}
+export const deleteList = api.delete<{ listId: string }>(
+  '/lists/:listId',
+  function* (ctx: ApiCtx<any, { listId: string }>, next): ApiGen {
+    const { listId } = ctx.payload;
+    yield next();
+    const { ok } = ctx.response;
+    if (!ok) return;
+    const itemIds = yield select(selectItemIdsByList, { id: listId });
+    ctx.actions.push(
+      removeLists([listId]),
+      removeListItemIds([listId]),
+      removeListItems(itemIds),
+      setLoaderSuccess({ id: ctx.name, message: listId }),
+    );
+  },
+);
 
 interface DeleteListItem {
   listId: string;
   itemId: string;
 }
 
-function* onDeleteListItem(payload: DeleteListItem): ApiGen {
-  const { itemId, listId } = payload;
-  const loader = deleteItemLoader(itemId);
+export const deleteListItem = api.delete<DeleteListItem>(
+  '/lists/:listId/items/:itemId',
+  function* (ctx: ApiCtx<any, DeleteListItem>, next): ApiGen {
+    // TODO: const loader = deleteItemLoader(itemId);
+    const { itemId, listId } = ctx.payload;
+    yield next();
+    const { ok } = ctx.response;
+    if (!ok) return;
 
-  yield put(setLoaderStart({ id: loader }));
-
-  const resp = yield call(apiFetch, `/lists/${listId}/items/${itemId}`, {
-    method: 'DELETE',
-  });
-
-  if (!resp.ok) {
-    yield put(setLoaderError({ id: loader }));
-    return;
-  }
-
-  const itemIds: string[] = yield select(selectItemIdsByList, {
-    id: payload.listId,
-  });
-  const newItemIds = itemIds.filter((id) => id !== itemId);
-  yield batch([
-    addListItemIds({ [listId]: newItemIds }),
-    setLoaderSuccess({ id: loader }),
-  ]);
-}
+    const itemIds: string[] = yield select(selectItemIdsByList, {
+      id: listId,
+    });
+    const newItemIds = itemIds.filter((id) => id !== itemId);
+    ctx.actions.push(addListItemIds({ [listId]: newItemIds }));
+  },
+);
 
 interface FetchList {
   username: string;
   listname: string;
 }
 
-export function* onFetchList({ username, listname }: FetchList) {
-  if (!username || !listname) {
-    return;
-  }
+export const fetchList = api.get<FetchList>(
+  '/lists/:username/:listname',
+  function* (ctx: ApiCtx<FetchListResponse, FetchList>, next) {
+    // TODO: const loader = fetchListLoader(username, listname);
+    if (!ctx.payload.username || !ctx.payload.listname) {
+      return;
+    }
+    yield next();
+    const { ok, data } = ctx.response;
+    if (!ok) return;
 
-  const loader = fetchListLoader(username, listname);
-  yield put(setLoaderStart({ id: loader }));
-  const resp: ApiFetchResponse<FetchListResponse> = yield call(
-    apiFetch,
-    `/lists/${username}/${listname}`,
-  );
+    const list = deserializeList(data.list);
+    const { items, itemIds } = processListItems(data.items);
+    const users = processUsers(data.users);
+    const comments = processComments(data.comments);
 
-  if (!resp.ok) {
-    yield put(setLoaderError({ id: loader, message: resp.data.message }));
-    return;
-  }
-
-  const list = deserializeList(resp.data.list);
-  const { items, itemIds } = processListItems(resp.data.items);
-  const users = processUsers(resp.data.users);
-  const comments = processComments(resp.data.comments);
-
-  yield batch([
-    addLists({ [list.id]: list }),
-    addListItems(items),
-    addListItemIds({ [list.id]: itemIds }),
-    addUsers(users),
-    addComments(comments),
-    setLoaderSuccess({ id: loader }),
-  ]);
-}
-
-export const {
-  fetchList,
-  deleteListItem,
-  deleteList,
-  orderListItems,
-  updateListItem,
-  updateList,
-  fetchLists,
-  createList,
-  createListItem,
-  createListItemBulk,
-  fetchPublicLists,
-  starList,
-  fetchStars,
-} = createEffects({
-  fetchList: onFetchList,
-  deleteListItem: onDeleteListItem,
-  deleteList: onDeleteList,
-  orderListItems: onOrderListItems,
-  updateListItem: onUpdateListItem,
-  updateList: onUpdateList,
-  fetchLists: onFetchLists,
-  createList: onCreateList,
-  fetchPublicLists: onFetchPublicLists,
-  createListItem: onCreateListItem,
-  starList: onStarList,
-  fetchStars: onFetchStars,
-  createListItemBulk: onCreateListItemBulk,
-});
+    ctx.actions.push(
+      addLists({ [list.id]: list }),
+      addListItems(items),
+      addListItemIds({ [list.id]: itemIds }),
+      addUsers(users),
+      addComments(comments),
+    );
+  },
+);
