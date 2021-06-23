90492e949689101e8307b933c9a28788197675de
Eric Bower <me@erock.io>
2021-06-22 22:06:12 -0400

    progress

---
+++ src/server/services/generate-image.ts
@@ -0,0 +1,55 @@
+import puppeteer from 'puppeteer';
+// import util from 'util';
+// import fs from 'fs';
+
+import { ListClient } from '@app/types';
+
+// const writeFile = util.promisify(fs.writeFile);
+
+interface TemplateData {
+  list: ListClient;
+}
+
+function compileTemplate(template: string, templateData: TemplateData): string {
+  let html = template;
+  Object.keys(templateData.list).forEach((key) => {
+    html = html.replace(`{{${key}}}`, (templateData.list as any)[key]);
+  });
+  return html;
+}
+
+const template = `<html>
+  <body>
+    {{name}}
+  </body>
+</html>`;
+
+export async function generateImage(data: TemplateData) {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+
+  // Render some HTML from the relevant template
+  const html = compileTemplate(template, data);
+
+  // Create a new page
+  const page = await browser.newPage();
+
+  // Set the content to our rendered HTML
+  await page.setContent(html, { waitUntil: 'domcontentloaded' });
+  const fname = `public/${data.list.username}_${data.list.urlName}.png`;
+
+  const screenshotBuffer = await page.screenshot({
+    fullPage: false,
+    type: 'png',
+    path: fname,
+  });
+
+  if (!screenshotBuffer) {
+    return '';
+  }
+
+  await page.close();
+
+  // await writeFile(fname, screenshotBuffer.toString());
+
+  return fname;
+}
