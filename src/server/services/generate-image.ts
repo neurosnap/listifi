316a0d43da910202824d87e31392a8e1a44d79c2
Eric Bower <me@erock.io>
2021-06-24 23:35:28 -0400

    wrap lines

--- src/server/services/generate-image.ts
+++ src/server/services/generate-image.ts
@@ -1,6 +1,11 @@
 import debug from 'debug';
 import { MapEntity } from 'robodux';
-import { createCanvas, loadImage, registerFont } from 'canvas';
+import {
+  createCanvas,
+  loadImage,
+  registerFont,
+  NodeCanvasRenderingContext2D,
+} from 'canvas';
 
 import { deserializeList, processListItems } from '@app/lists';
 import { processComments } from '@app/comments';
@@ -15,6 +20,41 @@ export interface TemplateData {
   list: ListClient;
   itemIds: string[];
   comments: MapEntity<ListCommentClient>;
+}
+
+function wrapLines(
+  ctx: NodeCanvasRenderingContext2D,
+  text: string,
+  maxWidth: number,
+) {
+  const lines = [];
+  let result = '';
+  let i = 0;
+  let j = 0;
+  let width = 0;
+
+  while (text.length) {
+    for (
+      i = text.length;
+      ctx.measureText(text.substr(0, i)).width > maxWidth;
+      i--
+    );
+
+    result = text.substr(0, i);
+
+    if (i !== text.length)
+      for (
+        j = 0;
+        result.indexOf(' ', j) !== -1;
+        j = result.indexOf(' ', j) + 1
+      );
+
+    lines.push(result.substr(0, j || result.length));
+    width = Math.max(width, ctx.measureText(lines[lines.length - 1]).width);
+    text = text.substr(lines[lines.length - 1].length, text.length);
+  }
+
+  return lines;
 }
 
 export async function generateListDetailImage(
@@ -53,14 +93,30 @@ export async function generateListDetailImage(
   ctx.fillStyle = '#fff';
   ctx.fillRect(0, 0, width, height);
   const marginX = 70;
+  const widthLength = width - marginX * 2;
 
-  ctx.font = 'bold 70px opensans';
+  const titleFontSize = 70;
+  ctx.font = `bold ${titleFontSize}px opensans`;
   ctx.fillStyle = '#3A3B3C';
-  ctx.fillText(list.name, marginX, 150);
+  const titleText = list.name;
+  const lines = wrapLines(ctx, titleText, widthLength).slice(0, 2);
+  for (let i = 0; i < lines.length; i += 1) {
+    const line = lines[i];
+    ctx.fillText(line, marginX, 150 + titleFontSize * i);
+  }
 
-  ctx.font = 'normal normal 30px opensans';
+  const subFontSize = 30;
+  ctx.font = `normal normal ${subFontSize}px opensans`;
   ctx.fillStyle = '#666';
-  ctx.fillText(list.description, marginX, 150 + 50 + 30);
+  const descLines = wrapLines(ctx, list.description, widthLength).slice(0, 2);
+  for (let i = 0; i < descLines.length; i += 1) {
+    const line = descLines[i];
+    ctx.fillText(
+      line,
+      marginX,
+      150 + lines.length * titleFontSize + (subFontSize + 10) * i,
+    );
+  }
 
   ctx.fillStyle = '#3A3B3C';
   const metricsY = 500;
